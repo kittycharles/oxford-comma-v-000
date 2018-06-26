c5f6c48dca20484d4f1753461f956640ef6cea71
Richard Sanchez <richard.charles.s@gmail.com>
2018-06-26 18:17:06 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,6 +4,6 @@ def oxford_comma(array)
   elsif array.length == 2
     array.join(" and ")
   elsif array.length > 2
-    array.join(", ") + array[-1].last << (", and ")
+    array.join(", ") + array.last << (", and ")
   end
 end
