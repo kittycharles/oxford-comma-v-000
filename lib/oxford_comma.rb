7c74dd52911d705dbe726f4ed9d629052b73ead2
Richard Sanchez <richard.charles.s@gmail.com>
2018-06-26 18:40:48 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,11 @@
 def oxford_comma(array)
-  if array.length ==1
+  case array.size
+  when 1
     array.join
-  elsif array.length == 2
-    array.join(" and ")
-  elsif array.length > 2
-    array.insert(-1, ", and") + array.join(", ")
+  when 2
+    return array.join(" and ")
+  else
+    array[-1].insert(0, "and ")
   end
+  array.join(", ")
 end
